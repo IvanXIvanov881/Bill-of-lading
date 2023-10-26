8e42073061cce7cb5d64818f6e77678f8fb9d1f7
IvanXIvanov881 <vankok8812@gmail.com>
2023-10-26 17:41:45 +0300

    Successfully entered
    message about successfully entered data in bill of lading

--- src/main.java
+++ src/main.java
@@ -19,11 +19,13 @@ public class main {
             String choice = getString(scanner);
 
             switch (choice) {
+
                 case "1" -> {
                     System.out.println("Изберете: 1 за чуплив товар");
                     System.out.println("Изберете: 2 за нечуплив товар");
                     System.out.println("__________________________________________");
                     String fragileOrNot = scanner.nextLine();
+
                     if (fragileOrNot.equals("1")) {
                         System.out.println("Номер на товарителница, килограми, обем, изпратена от кой град, адресирана до кой град.");
                         System.out.println("(Пример: A1234h 12.5 20 Русе Варна)");
@@ -31,22 +33,28 @@ public class main {
                         String[] input = scanner.nextLine().split("\\s+");
                         String firstNum = input[1];
                         String secondNum = input[2];
-                        if (extracted(firstNum) && extracted(secondNum)) {
+
+                        if (isDouble(firstNum) && isDouble(secondNum)) {
                             WayBill wayBill = new FragileLoad(input[0], Double.parseDouble(input[1]), Double.parseDouble(input[2]), input[3], input[4]);
                             controller.addWayBillToRepo(wayBill);
+                            System.out.printf("Вие успешно въведохте товарителница N:%s!%n",input[0]);
+                            System.out.println("__________________________________________");
                         } else {
                             System.out.println("Моля въведете на 2-ра и 3-та секция число!");
                             System.out.println("__________________________________________");
                         }
+
                     } else if (fragileOrNot.equals("2")) {
                         System.out.println("Номер на товарителница, килограми, обем, изпратена от кой град, адресирана до кой град.");
                         System.out.println("(Пример: A5324h 12.5 20 Русе Варна)");
                         String[] input = scanner.nextLine().split("\\s+");
                         String firstNum1 = input[1];
                         String secondNum1 = input[2];
-                        if (extracted(firstNum1) && extracted(secondNum1)) {
+                        if (isDouble(firstNum1) && isDouble(secondNum1)) {
                             WayBill wayBill = new UnbreakableLoad(input[0], Double.parseDouble(input[1]), Double.parseDouble(input[2]), input[3], input[4]);
                             controller.addWayBillToRepo(wayBill);
+                            System.out.printf("Вие успешно въведохте товарителница N:%s!%n",input[0]);
+                            System.out.println("__________________________________________");
                         } else {
                             System.out.println("Моля въведете на 2-ра и 3-та секция число!");
                             System.out.println("__________________________________________");
@@ -61,6 +69,7 @@ public class main {
                     String numberInput2 = scanner.nextLine();
                     System.out.println(controller.getWayStatistic(numberInput2));
                 }
+
                 case "3" -> {
                     System.out.println("Моля въведете номер на товарителницата:");
                     String numberInput3 = scanner.nextLine();
@@ -68,6 +77,7 @@ public class main {
                     System.out.printf("Текущата локация на товарителница номер:%s е %s%n", numberInput3, currentLocation);
                     System.out.println("__________________________________________");
                 }
+
                 case "4" -> {
                     System.out.println("Моля въведете номер на товарителницата:");
                     String numberInput4 = scanner.nextLine();
@@ -77,16 +87,19 @@ public class main {
                     System.out.printf("Вие успешно пренасочихте товарителница номер:%s към %s%n", numberInput4, newLocation);
                     System.out.println("__________________________________________");
                 }
+
                 case "5" -> {
                     double totalPrice = controller.sumOfAll();
                     System.out.printf("Сума: %.2fлв.\n", totalPrice);
                     System.out.println("__________________________________________");
                 }
+
                 case "6" -> {
                     int totalWayBills = controller.getStatistics();
                     System.out.printf("Брой създадени товарителници: %d.\n", totalWayBills);
                     System.out.println("__________________________________________");
                 }
+
                 case "7" -> {
                     System.out.println("Моля въведете номер на товарителницата:");
                     String numberInput5 = scanner.nextLine();
@@ -99,11 +112,13 @@ public class main {
                         System.out.println("__________________________________________");
                     }
                 }
+
                 case "8" -> {
                     System.out.println("Изход.");
                     System.out.println("__________________________________________");
                     exit = true;
                 }
+
                 default -> {
                     System.out.println("Невалиден избор!");
                     System.out.println("________________");
@@ -112,10 +127,9 @@ public class main {
         }
     }
 
-    private static boolean extracted(String firstNum) {
-
+    private static boolean isDouble(String numberToCheck) {
             try {
-                Double.parseDouble(firstNum);
+                Double.parseDouble(numberToCheck);
                 return true;
             } catch (NumberFormatException e) {
                 return false;
@@ -124,14 +138,14 @@ public class main {
 
     private static String getString(Scanner scanner) {
         String sb = "Моля изберете една от следните опции:" + System.lineSeparator() +
-                "1 - създаване на товарителница." + System.lineSeparator() +
-                "2 - преглед на товарителница." + System.lineSeparator() +
-                "3 - проследяване на товарителница." + System.lineSeparator() +
-                "4 - пренасочване на товарителница." + System.lineSeparator() +
+                "1 - Създаване на товарителница." + System.lineSeparator() +
+                "2 - Преглед на товарителница." + System.lineSeparator() +
+                "3 - Проследяване на товарителница." + System.lineSeparator() +
+                "4 - Пренасочване на товарителница." + System.lineSeparator() +
                 "5 - Обща цена на всички товарителници." + System.lineSeparator() +
                 "6 - Брой създадени товарителници." + System.lineSeparator() +
-                "7 - изтриване на товарителница." + System.lineSeparator() +
-                "8 - изход." + System.lineSeparator() +
+                "7 - Изтриване на товарителница." + System.lineSeparator() +
+                "8 - Изход." + System.lineSeparator() +
                 "__________________________________________" + System.lineSeparator();
         System.out.print(sb);
         return scanner.nextLine();
