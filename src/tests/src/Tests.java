0df7e8e22f7c4a824321f7779dd57647685d79be
IvanXIvanov881 <vankok8812@gmail.com>
2023-10-26 19:58:40 +0300

    New tests
    added more tests

--- src/tests/src/Tests.java
+++ src/tests/src/Tests.java
@@ -10,35 +10,49 @@ public class Tests {
 
     @Test
     public void testCreateFragileLoad(){
+        Controller controller = new Controller();
         WayBill wayBill = new FragileLoad("A123", 20, 30, "Dobrich", "Varna");
-        Controller contr = new Controller();
-        contr.addWayBillToRepo(wayBill);
-        Assert.assertEquals(1, contr.getStatistics());
+        controller.addWayBillToRepo(wayBill);
+        Assert.assertEquals(1, controller.getStatistics());
+    }
+
+    @Test (expected = ArrayIndexOutOfBoundsException.class)
+    public void testCreateEmptyBlank(){
+        Controller controller = new Controller();
+        String[] input = {""};
+        WayBill wayBill = new FragileLoad(input[0], Double.parseDouble(input[1]), Double.parseDouble(input[2]), input[3], input[4]);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNegativeVolume(){
+        Controller controller = new Controller();
+        WayBill wayBill = new FragileLoad("A123", 20, -30, "Dobrich", "Varna");
+        controller.addWayBillToRepo(wayBill);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNegativeKilograms(){
+        Controller controller = new Controller();
+        WayBill wayBill = new FragileLoad("A123", -20, 30, "Dobrich", "Varna");
+        controller.addWayBillToRepo(wayBill);
     }
 
     @Test
-    public void testEmptyBlank(){
-
+    public void testRemoveBlank(){
+        Controller controller = new Controller();
+        WayBill wayBill = new FragileLoad("A123", 20, 30, "Dobrich", "Varna");
+        controller.addWayBillToRepo(wayBill);
+        controller.removeWayBillToRepo("A123");
+        Assert.assertEquals(0, controller.getStatistics());
     }
 
-    @Test
-    public void testCreateBillOfLadingBlank(){
-
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistingBillOfLading(){
+        Controller controller = new Controller();
+        WayBill wayBill = new FragileLoad("A123", 20, 30, "Dobrich", "Varna");
+        controller.addWayBillToRepo(wayBill);
+        controller.getWayStatistic("DDD");
     }
 
-    @Test
-    public void testDifferentChoiceOfFragileAndUnbreakable(){
-
-    }
-
-    @Test
-    public void testLettersInBlankForVolume(){
-
-    }
-
-    @Test
-    public void testLettersInBlankForKilograms(){
-
-    }
 
 }
